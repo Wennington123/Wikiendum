db4392d25c2ff9b4cfd22aa56ec726cd3bb974e7
Wennington123 <wenningtondiasx25@gmail.com>
2026-05-04 15:57:23 -0300

    new_layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    // Lê tudo dentro da pasta app (onde estão nossas páginas de verdade)
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // Deixei preparado para quando criarmos componentes separados (como botões e a busca)
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
